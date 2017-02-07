783702f614b546911426567a8b60359453345fb0
DmitriyOsipov <osipov.dmal@gmail.com>
2017-02-07 08:34:44 +0200

    added calculations with array (Fibonacci sequence to entered number of element)

--- Lessons/src/Ex3/Fibo.java
+++ Lessons/src/Ex3/Fibo.java
@@ -15,8 +15,29 @@ public class Fibo {
             System.out.println("Enter number");
             num = userConsoleReader.readInt(0);
 
-            long Fn = Math.round(Math.pow(phi, num) / sqrt5);
-            System.out.println(num + " Fibonacci number is " + Fn);
+            System.out.println("Calculating Fn with Golden ratio");
+            long fn = Math.round(Math.pow(phi, num) / sqrt5);
+            System.out.println("\t" + num + " Fibonacci number is " + fn);
+
+            System.out.println("----------------------------------------");
+
+            System.out.println("Calculating all numbers to Fn");
+            long[] fibo = {0, 1};
+            long fnAll = 1;
+            System.out.print(fnAll + "  ");
+            for (int i=1; i<num; i++){
+                fnAll = fibo[0] + fibo[1];
+                fibo[0] = fibo[1];
+                fibo[1] = fnAll;
+                if (i%20==0){
+                    System.out.println();
+                }
+                System.out.print(fnAll + "  ");
+            }
+            System.out.println();
+            System.out.println();
+            System.out.println("\t" + num + " Fibonacci number is " + fn);
+            System.out.println("----------------------------------------");
         }
         while (userConsoleReader.isRepeatYes());
     }
