6fbd005f8eb802b8cf4c8bed2fa02689e391eb51
DmitriyOsipov <osipov.dmal@gmail.com>
2017-02-12 15:28:30 +0200

    recursive multiply

---
+++ Lessons/src/Ex4/MultiplyRecursive.java
@@ -0,0 +1,51 @@
+package Ex4;
+
+import utilClasses.UserConsoleReader;
+
+import java.math.BigInteger;
+
+public class MultiplyRecursive {
+    public static void main(String[] args) throws Exception{
+        UserConsoleReader userConsoleReader = new UserConsoleReader();
+        int firsNum, secondNum;
+        long result;
+
+        do {
+            System.out.println("Input first number");
+            firsNum = userConsoleReader.readInt(-1);
+            System.out.println("Input second number");
+            secondNum = userConsoleReader.readInt(-1);
+            result = multiply(firsNum, secondNum, firsNum);
+            System.out.println(result);
+            //System.out.println(multiply(BigInteger.valueOf(firsNum), BigInteger.valueOf(secondNum), BigInteger.valueOf(firsNum)));
+        }while (userConsoleReader.isRepeatYes());
+    }
+
+    public static long multiply(int x, int y, long a){
+        if ((x==0)||(y==0)){
+            return 0;
+        }
+        if ((x==1)){
+            return a=y;
+        }
+        if ((y==1)){
+            return a;
+        }
+        //*/
+        //a+=x;
+        return multiply(x, y-1, a+x);
+    }
+/*
+    public static BigInteger multiply(BigInteger x, BigInteger y, BigInteger a){
+        if (x.equals(BigInteger.ZERO) || y.equals(BigInteger.ZERO)){
+            return BigInteger.ZERO;
+        }
+        if (x.equals(BigInteger.ONE)){
+            return a = new BigInteger(y.toByteArray());
+        }
+        if (y.equals(BigInteger.ONE)){
+            return a;
+        }
+        return multiply(x, y.subtract(BigInteger.ONE), a.add(x));
+    }//*/
+}
