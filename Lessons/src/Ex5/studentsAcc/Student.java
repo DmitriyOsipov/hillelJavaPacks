df06e30c278c7c10db86218eb40b09034c41d5ac
DmitriyOsipov <osipov.dmal@gmail.com>
2017-02-15 16:44:43 +0200

    Students structure in work

---
+++ Lessons/src/Ex5/studentsAcc/Student.java
@@ -0,0 +1,47 @@
+package Ex5.studentsAcc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by mtzadmin on 15.02.2017.
+ */
+public class Student extends Person{
+    private HashMap<String, Subject> disciplines;
+
+    public Student(int id, String name, String surname, int year, int birthMonth, int birthDay){
+        super(id, name, surname, year, birthMonth, birthDay);
+        disciplines = new HashMap<>();
+    }
+
+    public void addDiscipline(String name){
+        this.disciplines.put(name, new Subject(name, Subject.Status.STUDYING));
+    }
+
+    public double getAverageMark(String disciplineName){
+        double result = 0;
+        if ((disciplineName == null)||(disciplineName.length()==0)) {
+            for (Subject subject: disciplines.values()){
+                result+=subject.getAverageMark();
+            }
+            result = result/disciplines.values().size();
+        }
+        else {
+            result = disciplines.get(disciplineName).getAverageMark();
+        }
+        return result;
+    }
+
+    public String getSkipsToString(){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Subject subject: disciplines.values()){
+            if (subject.getStatus() == Subject.Status.STUDYING){
+                stringBuilder.append(subject.getName() + "\n");
+                stringBuilder.append(subject.skipsToString() + "\n");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
