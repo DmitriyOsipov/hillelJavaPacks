d65de587713c8aee75f749c76065a4fdd087e0cd
DmitriyOsipov <osipov.dmal@gmail.com>
2017-02-16 15:18:49 +0200

    Students structure changed in work

---
+++ Lessons/src/Ex5/studentsAccounting/Skip.java
@@ -0,0 +1,7 @@
+package Ex5.studentsAccounting;
+
+/**
+ * Created by mtzadmin on 16.02.2017.
+ */
+public class Skip {
+}
---
+++ Lessons/src/Ex5/studentsAccounting/Student.java
@@ -0,0 +1,21 @@
+package Ex5.studentsAccounting;
+
+import java.util.HashMap;
+
+/**
+ * Created by mtzadmin on 16.02.2017.
+ */
+public class Student extends Person {
+    private HashMap<String, Subject> disciplines;
+
+    public Student(int id, String name, String surname, int year, int birthMonth, int birthDay){
+        super(id, name, surname, year, birthMonth, birthDay);
+        disciplines = new HashMap<>();
+    }
+
+    public double getAverageMark(){
+        double result=0;
+
+        return result;
+    }
+}
