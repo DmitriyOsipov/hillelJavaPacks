4cc4f9bbee5357a62e1930ee35528ac6ed4592d7
DmitriyOsipov <osipov.dmal@gmail.com>
2017-03-20 08:26:45 +0200

    tic-tac-toe

--- Lessons/src/Ex10/Playable.java
+++
@@ -1,5 +0,0 @@
-package Ex10;
-
-public interface Playable {
-    public boolean makeMove(int[][] field);
-}
--- Lessons/src/Ex10/ticTacToeGame/AiGameStrategy.java
+++ Lessons/src/Ex10/ticTacToeGame/AiGameStrategy.java
@@ -1,9 +1,6 @@
 package Ex10.ticTacToeGame;
-/**
- * Created by VSulevskiy on 19.07.2015.
- */
+
 public interface AiGameStrategy {
 
-    //TODO implement this interface
     int aiMakeTurn(int[] field);
 }
