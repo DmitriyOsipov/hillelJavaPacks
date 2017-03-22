4c34eeff6f77c7d017deb84dafacdf6099c60877
DmitriyOsipov <osipov.dmal@gmail.com>
2017-03-22 09:50:29 +0200

    Simple hash map, implements map with generics

---
+++ Lessons/src/Ex12/MyHashMapGenericTypes/MyHashMap.java
@@ -0,0 +1,200 @@
+package Ex12.MyHashMapGenericTypes;
+
+import java.util.*;
+
+public class MyHashMap<K, V> implements Map<K, V>{
+    private final int PRIMARY_CAPACITY = 16;
+    private int capacity = PRIMARY_CAPACITY;
+    private int size = 0;
+    private List<Entry<K,V>>[] table = new LinkedList[capacity];
+
+    private int calculateIndex(K key){
+        return  Math.abs(key.hashCode()) % capacity;
+    }
+
+    private boolean addEntry(K key, V value){
+        int index = calculateIndex(key);
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        boolean result = false;
+
+        if (table[index] == null){
+            table[index] = new LinkedList<Entry<K, V>>();
+        }
+        if (!table[index].contains(newEntry)){
+            table[index].add(newEntry);
+        }
+        else {
+            int existedIndex = table[index].indexOf(newEntry);
+            table[index].get(existedIndex).setValue(value);
+        }
+        result = true;
+        if (result){
+            size++;
+        }
+        return result;
+    }
+
+    private Entry<K, V> getEntry(K key){
+        int bucketIndex = calculateIndex(key);
+        int index = table[bucketIndex].indexOf(new Entry<K, V>(key, null));
+        return (index>-1) ? table[bucketIndex].get(index) : null;
+    }
+
+    private Entry removeEntry(K key){
+        Entry result = null;
+        int bucketIndex = calculateIndex(key);
+        int index = table[bucketIndex].indexOf(new Entry<>(key, null));
+        if (index>-1){
+            result = table[bucketIndex].remove(index);
+            if (result!=null){
+                size--;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        int bucketIndex = calculateIndex((K)key);
+        return table[bucketIndex].contains(new Entry(key, null));
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for(List<Entry<K,V>> list: table){
+            for (Entry<K,V> entry : list){
+                if (entry.getValue().equals(value)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        K castedKey = (K)key;
+        Entry<K, V> entry = this.getEntry(castedKey);
+        return (entry!=null) ? entry.getValue() : null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        Entry<K, V> entry = this.getEntry(key);
+        this.addEntry(key, value);
+        return (entry!=null)? entry.getValue() : null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        K castedKey = (K)key;
+        Entry<K, V> removed = this.removeEntry(castedKey);
+        return (removed!=null) ? removed.getValue() : null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        for (Object entry: m.entrySet()){
+            Entry<K, V> obj = (Entry<K, V>)entry;
+            this.addEntry(obj.getKey(), obj.getValue());
+        }
+    }
+
+    @Override
+    public void clear() {
+        this.size = 0;
+        for (int i=0; i<capacity; i++){
+            table[i] = null;
+        }
+    }
+
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<K>();
+        for (int i=0; i<capacity; i++){
+            for (Entry<K, V> entry : table[i]){
+                set.add(entry.getKey());
+            }
+        }
+        return set;
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<Map.Entry<K, V>> set = new HashSet<>();
+        for (int i=0; i<capacity; i++){
+            for (Entry<K, V> entry : table[i]){
+                set.add(entry);
+            }
+        }
+        return set;
+    }
+
+    @Override
+    public Collection<V> values() {
+        Collection<V> vCollection = new LinkedList<V>();
+        for (int i=0; i<capacity; i++){
+            for (Entry<K, V> entry : table[i]){
+                vCollection.add(entry.getValue());
+            }
+        }
+        return vCollection;
+    }
+
+
+
+    private class Entry<K, V> implements Map.Entry<K, V>{
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+
+            Entry<?, ?> entry = (Entry<?, ?>) object;
+
+            return key != null ? key.equals(entry.key) : entry.key == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return key != null ? key.hashCode() : 0;
+        }
+
+        @Override
+        public String toString() {
+            return '{' + "key=" + key + ", value=" + value + '}';
+        }
+    }
+}
